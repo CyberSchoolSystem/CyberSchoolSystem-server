a1690ac8823bc754ae0427ff9359a054f1b3e0f6
Fabian Geiselhart <me@f4814n.de>
2018-03-27 14:27:32 +0200

    Accept JSON Requests

--- src/Handler/Vote.hs
+++ src/Handler/Vote.hs
@@ -6,10 +6,12 @@ module Handler.Vote
     , postVoteRemoveR
     ) where
 
+-- TODO: Cleanup Import list
 import           Database.Persist         ((==.), (=.), (+=.))
 import           Database.Persist.Types   (Filter, Entity(..), entityKey, updateValue)
 import           Database.Persist.Class   (selectFirst, selectList, insert, update)
 import           Database.Persist.MongoDB (nestEq, (->.), push, nestInc, eachOp)
+import           Data.Aeson
 import           Data.Maybe               (catMaybes)
 import qualified Data.Text as T
 import           Data.Text                (Text)
@@ -17,10 +19,9 @@ import           Foundation
 import           Model
 import           Text.Blaze.Html          (Html)
 import           Yesod.Persist.Core       (runDB)
-import           Yesod.Form.Input         (FormInput, iopt, ireq)
-import           Yesod.Form.Fields        (intField, textField)
-import           Yesod.Form.Input         (runInputGet, runInputPost)
 import           Yesod.Core.Handler       (invalidArgs)
+import           Yesod.Core.Json          (FromJSON, parseJsonBody)
+import           Data.Aeson.Types         (Result(..), Object(..), typeMismatch)
 
 data VoteReq = VoteReq
     { reqVoteId   :: Maybe Int
@@ -33,24 +34,24 @@ data VoteAct = VoteAct
     , actChoiceId :: Int
     } deriving (Show)
 
--- TODO: If this gets a UI integration a AForm would be useful
-{-| Parse input on voteInfoR into a VoteReq -}
-voteReqIForm :: FormInput Handler VoteReq
-voteReqIForm = VoteReq -- TODO: Negative ID check
-    <$> iopt intField "id"
-    <*> iopt intField "choice"
+instance FromJSON VoteReq where
+    parseJSON (Object v) = VoteReq
+        <$> v .:? "id"
+        <*> v .:? "choice"
+    parseJSON invalid = typeMismatch "VoteReq" invalid
 
-voteActIForm :: FormInput Handler VoteAct
-voteActIForm = VoteAct
-    <$> ireq textField "chip"
-    <*> ireq intField "id"
-    <*> ireq intField "choice"
+instance FromJSON VoteAct where
+    parseJSON (Object v) = VoteAct
+        <$> v .: "chip"
+        <*> v .: "id"
+        <*> v .: "choice"
+    parseJSON invalid = typeMismatch "VoteAct" invalid
 
 {-| Handle GET on /api/vote/info -}
 -- TODO: Show as JSON
 getVoteInfoR :: Handler Text
 getVoteInfoR = do
-    inp <- runInputGet voteReqIForm
+    (Success inp) <- parseJsonBody :: Handler (Result VoteReq) -- TODO: Check Application Type?
     rec <- runDB $ selectList (reqToDBFilter inp) []
     return $ T.pack $ show rec
 
@@ -63,14 +64,14 @@ reqToDBFilter req = catMaybes go
 {-| Handle POST on /api/vote/vote -}
 postVoteActR :: Handler ()
 postVoteActR = do
-    inp <- runInputPost voteActIForm
+    (Success inp) <- parseJsonBody :: Handler (Result VoteAct) -- TODO: Check for malformed JSON
     user <- runDB $ selectFirst [UserChipId ==. chip inp] []
     vote <- runDB $ selectFirst [VoteIdentity ==. actVoteId inp,
                                  VoteChoices ->. ChoiceIdentity `nestEq` actChoiceId inp] []
     case (user, vote) of
         (Just u, Just v) -> if isAllowed u v
                                 then updateVote u v inp
-                                else invalidArgs ["You already voted"]
+                                else invalidArgs ["You already voted"] -- TODO: permissionDenied
         _                -> invalidArgs ["User or Vote not found"]
 
 {-| Vote. Update all neccessary records -}
@@ -80,6 +81,7 @@ updateVote user vote inp = do
     runDB $ update (entityKey vote) [push VoteVoted (entityKey user)]
     runDB $ update (entityKey vote) [VoteChoices =. updateChoices inp choices]
 
+{-| Insert Description -}
 updateChoices :: VoteAct -> [Choice] -> [Choice]
 updateChoices VoteAct{actChoiceId = cid} = map inc
     where inc x = if choiceIdentity x == cid
