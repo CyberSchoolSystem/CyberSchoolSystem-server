19a81c8ab5bad4469c789ba46d3acdd70c927963
Fabian Geiselhart <me@f4814n.de>
2018-03-28 20:28:05 +0200

    Implement postApiInR and postApiOutR

--- src/Model.hs
+++ src/Model.hs
@@ -4,15 +4,23 @@
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE FlexibleInstances          #-}
 
 module Model where
 
 import Data.ByteString            (ByteString)
+import Data.Time.Clock            (UTCTime)
+import Data.Text                  (Text)
 import Database.Persist.MongoDB
 import Database.Persist.Quasi
 import Database.Persist.TH
-import Data.Text                  (Text)
 import Language.Haskell.TH.Syntax (Type (ConT))
+
 
 let mongoSettings = mkPersistSettings (ConT ''MongoContext) -- TODO: Default Settings?!?!
  in share [mkPersist mongoSettings] $(persistFileWith upperCaseSettings "config/models")
+
+
+instance Ord Access where
+    compare x y = accessTime x `compare` accessTime y
+    x <= y      = accessTime x <= accessTime y
