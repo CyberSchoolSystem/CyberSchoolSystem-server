c52a8f2e51a4cf2fb949446970c6de85b4de53b6
Fabian Geiselhart <me@f4814n.de>
2018-04-07 20:47:36 +0200

    Add user search ui and add Jquery to default layout

--- src/Handler/Dashboard.hs
+++ src/Handler/Dashboard.hs
@@ -4,25 +4,37 @@
 module Handler.Dashboard
     ( getDashboardR
     , getUiUserAddR
+    , getUiUserInfoR
     ) where
 
 import           Foundation
+import           Data.Monoid        ((<>))
 import           Settings           (appReload)
 import           Text.Blaze.Html    (Html)
 import           Text.Julius        (juliusFile, juliusFileReload)
-import           Text.Hamlet        (hamlet, hamletFile)
+import           Text.Hamlet        (hamlet, hamletFile, hamletFileReload)
 import           Yesod.Core         (defaultLayout)
 import           Yesod.Core.Handler (getYesod)
-import           Yesod.Core.Widget  (toWidget, addScriptRemote)
+import           Yesod.Core.Widget  (toWidget, setTitle)
 
 getUiUserAddR :: Handler Html
 getUiUserAddR = defaultLayout $ do
-    addScriptRemote "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.js"
+    setTitle "User add"
     app <- getYesod
-    toWidget $(hamletFile "templates/addUser.hamlet")
+    toWidget $(hamletFile "templates/userAdd.hamlet")
     if appReload . appSettings $ app
-        then toWidget $(juliusFileReload "templates/addUser.julius")
-        else toWidget $(juliusFile "templates/addUser.julius")
+        then toWidget $(juliusFileReload "templates/userAdd.julius")
+        else toWidget $(juliusFile "templates/userAdd.julius")
+
+getUiUserInfoR :: Handler Html
+getUiUserInfoR = defaultLayout $ do
+    setTitle "User search"
+    app <- getYesod
+    if appReload . appSettings $ app
+        then toWidget $(juliusFileReload "templates/userInfo.julius")
+             <> toWidget $(hamletFileReload "templates/userInfo.hamlet")
+        else toWidget $(juliusFile "templates/userInfo.hamlet")
+             <> toWidget $(hamletFileReload "templates/userInfo.hamlet")
 
 getDashboardR :: Handler Html
 getDashboardR = defaultLayout $ toWidget
@@ -40,4 +52,5 @@ getDashboardR = defaultLayout $ toWidget
         <h1> Admin Stuff
         <ul>
             <li> <a href=@{UiUserAddR}>User add
+            <li> <a href=@{UiUserInfoR}>User search
     |]
