612dc02768ed46d1dc15826760aab9ef8fa3910e
Henning Klatt <henning@Hennings-MBP.fritz.box>
2018-04-17 19:50:17 +0200

    Menu and user add improvments

--- src/Handler/Joke.hs
+++ src/Handler/Joke.hs
@@ -12,8 +12,8 @@ import           Data.Text()
 
 getJokeR :: Handler ()
 getJokeR = do
-    _ <- runDB . insert $ User "Fabian" "Geiselhart" "10c" "JESUS"
+    _ <- runDB . insert $ User "Henning" "Klatt" "10c" "JESUS"
               (Just "sha256|17|pZUkwz0cnbtcVxSJBD3qeQ==|D3KVx2A/tAhcy44z2QjcG/FvoV2jEyvKm1nH+dgv0bw=")
               0 [] Role{roleCitizen = True, roleRepresentative = True,
-                        roleTeacher = Nothing, roleAdmin = False}
+                        roleTeacher = Nothing, roleAdmin = True}
     redirect RootR
