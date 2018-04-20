e98982e91e95ce2c7b2e395b72907ed51094aa82
Fabian Geiselhart <me@f4814n.de>
2018-04-20 20:36:54 +0200

    Add favicon

--- src/Handler/Root.hs
+++ src/Handler/Root.hs
@@ -1,14 +1,18 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE QuasiQuotes     #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell    #-}
+{-# LANGUAGE QuasiQuotes        #-}
+{-# LANGUAGE OverloadedStrings  #-}
 module Handler.Root
     ( getRootR
+    , getFaviconR
     ) where
 
+import           Data.FileEmbed     (embedFile)
 import           Foundation
-import           Text.Blaze.Html (Html)
-import           Yesod.Core.Widget (whamlet)
-import           Yesod.Core (defaultLayout)
+import           Text.Blaze.Html    (Html)
+import           Yesod.Core         (defaultLayout)
+import           Yesod.Core.Content (TypedContent(..), toContent)
+import           Yesod.Core.Handler (cacheSeconds)
+import           Yesod.Core.Widget  (whamlet)
 
 getRootR :: Handler Html
 getRootR = do
@@ -17,3 +21,8 @@ getRootR = do
             <h1> WELCOME TO THE SHOW!
             <p> Not implemented
         |]
+
+getFaviconR :: Handler TypedContent
+getFaviconR = do cacheSeconds $ 60 * 60 * 24 * 30 -- cache a month
+                 return $ TypedContent "image/x-icon"
+                        $ toContent $(embedFile "config/favicon.ico")
