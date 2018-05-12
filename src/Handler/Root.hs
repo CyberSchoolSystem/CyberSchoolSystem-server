0036b50cb5dfb1d9c61d2d8b67283ff1376ef4ec
Fabian Geiselhart <me@f4814n.de>
2018-05-12 16:50:39 +0200

    Comment out favicon

--- src/Handler/Root.hs
+++ src/Handler/Root.hs
@@ -3,22 +3,22 @@
 {-# LANGUAGE OverloadedStrings  #-}
 module Handler.Root
     ( getRootR
-    , getFaviconR
+    -- , getFaviconR
     ) where
 
-import           Data.FileEmbed     (embedFile)
+-- import           Data.FileEmbed     (embedFile)
 import           Foundation
 import           Text.Blaze.Html    (Html)
 import           Yesod.Core         (defaultLayout)
-import           Yesod.Core.Content (TypedContent(..), toContent)
-import           Yesod.Core.Handler (cacheSeconds)
+-- import           Yesod.Core.Content (TypedContent(..), toContent)
+-- import           Yesod.Core.Handler (cacheSeconds)
 import           Yesod.Core.Widget  (setTitle)
 
 getRootR :: Handler Html
 getRootR = defaultLayout $ do
     setTitle "Startseite"
 
-getFaviconR :: Handler TypedContent
-getFaviconR = do cacheSeconds $ 60 * 60 * 24 * 30 -- cache a month
-                 return $ TypedContent "image/x-icon"
-                        $ toContent $(embedFile "config/favicon.ico")
+-- getFaviconR :: Handler TypedContent
+-- getFaviconR = do cacheSeconds $ 60 * 60 * 24 * 30 -- cache a month
+--                  return $ TypedContent "image/x-icon"
+--                         $ toContent $(embedFile "config/favicon.ico")
