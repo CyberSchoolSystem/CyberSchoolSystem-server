5e1f4d34ef15dac25dd755153019f76e80d52298
Henning Klatt <henning.klatt@web.de>
2018-05-13 16:09:05 +0200

    New Library

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -15,7 +15,6 @@ import           Database.Persist.Class    (selectFirst)
 import           Database.Persist.MongoDB
 import           Database.Persist.Types    (Entity(..))
 import qualified Error                     as E
-import qualified Message                   as M
 import           Model                     (UserId, EntityField (..), Unique(..), User(..), Role(..))
 import           Paths_CyberSchoolSystem_Server (version)
 import           Settings
@@ -137,6 +136,7 @@ instance Yesod App where -- TODO: SSL
                         addScript (StaticR js_demo_js)
                         addScript (StaticR js_morris_min_js) -- Pie Chart
                         addScript (StaticR js_raphael_min_js) -- Pie Chart
+                        addScript (StaticR js_chart_min_js)
                         css
                         toWidget $(juliusFile "templates/defaultLayout.julius")
                         contents
@@ -144,10 +144,7 @@ instance Yesod App where -- TODO: SSL
         withUrlRenderer $(hamletFile "templates/defaultLayout.hamlet")
 
 instance RenderMessage App FormMessage where
-    renderMessage _ _ = defaultFormMessage 
-
-instance RenderMessage App M.Message where
-    renderMessage _ _ = M.fromMessage
+    renderMessage _ _ = defaultFormMessage -- No Translation TODO: Maybe add translation
 
 instance YesodAuth App where
     type AuthId App = UserId
@@ -241,7 +238,6 @@ checkAuth  role failText (Just u) = do
         else return $ Unauthorized failText
 checkAuth _ _ Nothing = return AuthenticationRequired
 
--- TODO: Convert to Message
 customErrorHandler :: ErrorResponse -> Handler TypedContent
 customErrorHandler NotFound = selectRep $ do
     provideRep $ defaultLayout $ do
