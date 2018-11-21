5fdcdcc58d2eb8e5d9cd2ccd1f80ae3be20a9349
Fabian Geiselhart <me@f4814n.de>
2018-11-21 21:35:07 +0100

    Add root template

--- src/Handler/Root.hs
+++ src/Handler/Root.hs
@@ -6,17 +6,25 @@ module Handler.Root
     -- , getFaviconR
     ) where
 
--- import           Data.FileEmbed     (embedFile)
 import           Foundation
+import           Data.Monoid        ((<>))
+import           Settings           (appReload)
 import           Text.Blaze.Html    (Html)
+import           Text.Julius        (juliusFile, juliusFileReload)
+import           Text.Hamlet        (hamletFile, hamletFileReload)
 import           Yesod.Core         (defaultLayout)
--- import           Yesod.Core.Content (TypedContent(..), toContent)
--- import           Yesod.Core.Handler (cacheSeconds)
-import           Yesod.Core.Widget  (setTitle)
+import           Yesod.Core.Handler (getYesod)
+import           Yesod.Core.Widget  (toWidget, setTitle)
 
 getRootR :: Handler Html
 getRootR = defaultLayout $ do
     setTitle "Startseite"
+    app <- getYesod
+    if appReload . appSettings $ app
+        then toWidget $(juliusFileReload "templates/root.julius")
+             <> toWidget $(hamletFileReload "templates/root.hamlet")
+        else toWidget $(juliusFile "templates/root.julius")
+             <> toWidget $(hamletFile "templates/root.hamlet")
 
 -- getFaviconR :: Handler TypedContent
 -- getFaviconR = do cacheSeconds $ 60 * 60 * 24 * 30 -- cache a month
